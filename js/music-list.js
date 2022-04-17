af34214e4011555bd8d054e17aac6a69079e692b
Muradspb <alikgrand55@gmail.com>
2022-04-17 14:30:18 +0500

    mymusiqz

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "City of sin",
+    artist: "Nbhd Nick",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "PETRUNKO",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "INDUSTRY BABY ",
+    artist: "LIl nas X",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Пустота",
+    artist: "JONY",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "WHOOPY",
+    artist: "",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "ЛАЛИ",
+    artist: "JONY",
+    img: "music-6",
+    src: "music-6"
+  },
+];
